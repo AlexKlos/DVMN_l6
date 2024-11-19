13c511048e21573896f0249308a3a430754b1760
AlexKlos <klos@list.ru>
2024-11-19 21:21:12 +0700

    Added empty lines between functions

--- main.py
+++ main.py
@@ -4,20 +4,26 @@ import urwid
 def has_digit(password):
     return any(char.isdigit() for char in password)
 
+
 def has_letters(password):
     return any(char.isalpha() for char in password)
+
 
 def has_upper_letters(password):
     return any(char.isupper() for char in password)
 
+
 def has_lower_letters(password):
     return any(char.islower() for char in password)
+
 
 def has_symbols(password):
     return any(not char.isdigit() and not char.isalpha() for char in password)
 
+
 def is_very_long(password):
     return len(password) > 12
+
 
 def password_rate(password):
     score = 0
@@ -34,8 +40,10 @@ def password_rate(password):
             score = score + 2
     return score
 
+
 def on_ask_change(edit, new_edit_text):
     reply.set_text("Рейтинг этого пароля: %s" % password_rate(new_edit_text))
+
 
 def main():
     ask = urwid.Edit('Введите пароль: ', mask='*')
